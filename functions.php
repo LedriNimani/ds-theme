5fc0bc092f891ed22e8981fed5243d377523d739
LedriNimani <ledrinimani2018@gmail.com>
2025-02-13 17:49:30 +0100

    Initial commit

---
+++ functions.php
@@ -0,0 +1,12 @@
+<?php
+   function add_theme_script(){
+    wp_enqueue_style("style", get_template_directory_uri()."/style.css",  false, "1.0", "all" );
+    wp_enqueue_scripts("main",get_template_directory_uri()."js/main.js", array('jquery'),"1.0", true);
+   }
+
+
+   add_action("wp_enqueue_scripts","add_theme_script");
+
+
+
+?>
