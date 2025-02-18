f4e17cd622e590423fb227c212c266827de57cd1
LedriNimani <ledrinimani2018@gmail.com>
2025-02-18 17:48:10 +0100

    ....

--- functions.php
+++ functions.php
@@ -2,6 +2,10 @@
    function add_theme_script(){
     wp_enqueue_style("style", get_template_directory_uri()."/style.css",  false, "1.0", "all" );
     wp_enqueue_scripts("main",get_template_directory_uri()."js/main.js", array('jquery'),"1.0", true);
+
+    if(is_singular() && get_option('thread-comments')){
+      wp_enqueue_script('comment-reply');
+    }
    }
 
 
