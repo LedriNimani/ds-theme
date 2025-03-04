24ce0964fa5a9c9a6af4b84754f1a8510187b521
LedriNimani <ledrinimani2018@gmail.com>
2025-03-04 17:47:20 +0100

    ....

--- functions.php
+++ functions.php
@@ -1,9 +1,12 @@
 <?php
    function add_theme_script(){
     wp_enqueue_style("style", get_template_directory_uri()."/style.css",  false, "1.0", "all" );
-    wp_enqueue_scripts("main",get_template_directory_uri()."js/main.js", array('jquery'),"1.0", "");
-    wp_enqueue_scripts("main",get_template_directory_uri()."js/main.js", "1.0", true);
-    wp_enqueue_scripts("main",get_template_directory_uri()."js/main.js", array('jquery'),"1.0", true);
+    wp_enqueue_style("main",get_template_directory_uri()."/css/main.css", false,"1.0", "all");
+    wp_enqueue_style("bootstrap.min",get_template_directory_uri()."/css/bootstrap.min.css",false "1.0", "all");
+
+
+    wp_enqueue_script("main",get_template_directory_uri()."/js/main.js", array('jquery'),"1.0", true);
+    wp_enqueue_script("bootstrap.bundle.min",get_template_directory_uri()."/js/bootstrap.bundle.min.js", array('jquery'),"1.0", true);
 
 
 
--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 
 
 <h1> Hello </h1>
+
+<div>
 <?php  
   if(have_posts()):
     while(have_posts()): the_post();
@@ -20,6 +22,44 @@
 
 <?php endwhile; ?>
 <?php endif;?>
+  </div>
+
+<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+<div class="carousel-inner">
+<div class="carousel-item active">
+
+<img class= "d-block w-100" src="<?php bloginfo("template_directory");?>/img/slide.png" alt="First Slide">
+</div>
+<div class="carousel-item">
+
+<img src="" alt="Second slide">
+</div>
+<div class="carousel-item ">
+
+<img src="" alt="Third slide">
+</div>
+</div>
+<a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+  <span class="sr-only">Previous</span>
+</a>
+<a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+<span class="carousel-control-next-icon" aria-hidden="true"></span>
+<span class="sr-only">Next</span>
+</a>
+
+
+</div>
+
+
+
+
+
+
+
+
+
+
 
 
 
